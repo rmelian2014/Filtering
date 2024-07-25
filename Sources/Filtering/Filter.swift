83e27031c560e52107eb04f03979e63761859b77
Reinier Melian <rmelian2014@gmail.com>
2024-07-25 20:30:41 +0200

    making public some classes and functions

--- Sources/Filtering/Filter.swift
+++ Sources/Filtering/Filter.swift
@@ -4,15 +4,15 @@
 //  Created by Reinier Melian Massip on 1/29/20.
 //
 
-protocol Filter {
+public protocol Filter {
 	associatedtype T
 	func filter<Spec: Specification>(_ items: [T], _ spec: Spec) -> [T]
 		where Spec.T == T
 }
 
-class FilterImpl<T>: Filter {
-
-	func filter<Spec: Specification>(_ items: [T], _ spec: Spec) -> [T]
+public class FilterImpl<T>: Filter {
+    public init() {} 
+    public func filter<Spec: Specification>(_ items: [T], _ spec: Spec) -> [T]
 		where Spec.T == T {
 		return items.filter({ spec.isSatisfied(item: $0)})
 	}
--- Sources/Filtering/Specification.swift
+++ Sources/Filtering/Specification.swift
@@ -4,61 +4,41 @@
 //  Created by Reinier Melian Massip on 1/29/20.
 //
 
-protocol Specification {
+public protocol Specification {
 	associatedtype T
 	func isSatisfied(item: T) -> Bool
 }
 
-class AndSpecification<T,
+public class AndSpecification<T,
 	SpecA: Specification,
 	SpecB: Specification>: Specification
 	where SpecA.T == SpecB.T, T == SpecA.T {
 	private let firstSpec: SpecA
 	private let seconSpec: SpecB
 
-	init(_ first: SpecA, _ second: SpecB) {
+    public init(_ first: SpecA, _ second: SpecB) {
 		firstSpec = first
 		seconSpec = second
 	}
 
-	func isSatisfied(item: T) -> Bool {
+    public func isSatisfied(item: T) -> Bool {
 		return firstSpec.isSatisfied(item: item) && seconSpec.isSatisfied(item: item)
 	}
 }
 
-class OrSpecification<T,
+public class OrSpecification<T,
 	SpecA: Specification,
 	SpecB: Specification>: Specification
 	where SpecA.T == SpecB.T, T == SpecA.T {
 	private let firstSpec: SpecA
 	private let secondSpec: SpecB
 
-	init(_ first: SpecA, _ second: SpecB) {
+    public init(_ first: SpecA, _ second: SpecB) {
 		firstSpec = first
 		secondSpec = second
 	}
 
-	func isSatisfied(item: T) -> Bool {
+    public func isSatisfied(item: T) -> Bool {
 		return firstSpec.isSatisfied(item: item) || secondSpec.isSatisfied(item: item)
-	}
-}
-
-protocol AssetSearchable: Asset {
-    var searchableTexts: [String] { get }
-}
-
-class AssetContaintText: Specification {
-	typealias T = AnyAsset
-
-	private let text: String
-	init(_ text: String) {
-		self.text = text
-	}
-
-	func isSatisfied(item: T) -> Bool {
-        guard !item.asset.searchableTexts.isEmpty else {
-            return false
-        }
-        return !item.asset.searchableTexts.filter({ $0.contains(text) }).isEmpty
 	}
 }
